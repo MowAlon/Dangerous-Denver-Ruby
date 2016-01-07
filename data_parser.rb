5a3ca929d24984248642c3ff91929381c31bdc66
Alon Waisman <mowalon@yahoo.com>
2016-01-07 09:42:49 -0700

    Use .tap in parser and remove limitation of first 5 records

--- data_parser.rb
+++ data_parser.rb
@@ -3,11 +3,11 @@ require 'csv'
 class DataParser
 
   def self.converted_data(filename, *fields)
-    dataset = []
-    CSV.foreach(filename, headers: true, :header_converters => :symbol) do |row|
-      dataset << row.to_hash.keep_if{|key, value| fields.include?(key)} unless row[:is_traffic] == '1'
+    [].tap do |dataset|
+      CSV.foreach(filename, headers: true, :header_converters => :symbol) do |row|
+        dataset << row.to_hash.keep_if{|key, value| fields.include?(key)} unless row[:is_traffic] == '1'
+      end
     end
-    dataset
   end
 
 end
